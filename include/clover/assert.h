442ea45bc3e4bdf76eb59f65fbeb9933691b8cea
Thaynan Silva <thaynan.msilva@proton.me>
2025-05-26 23:10:34 -0300

    Assert: improve assert structure

--- include/clover/assert.h
+++ include/clover/assert.h
@@ -4,6 +4,12 @@
 #include <clover/base.h>
 #include <clover/log.h>
 
-#define clv_assert(expr,tail)   if (!(expr)) { clv_error ("assertion failed: " #expr); tail; }
+#define clv_assert(expr,_tail) \
+    do { \
+        if (!(expr)) { \
+            clv_error ("assertion failed: " #expr); \
+            _tail; \
+        } \
+    } while (0)
 
 #endif /* CLOVER_ASSERT_H_ */
